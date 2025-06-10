e3add975ed84f8c34972275eb6eb858cad4793ad
Keya <keyamondal549@gmail.com>
2025-06-10 20:56:51 +0600

    integrated nodemailer for senfding mail & mailtrap as email delivery platform

---
+++ Ai-Guidance-Assistant/Utilities/mailer.js
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer'
+
+export const sendMail=async(to,subject,text)=>{
+try{
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.AILTRAP_SMTP_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.AILTRAP_SMTP_USER,
+            pass: process.env.AILTRAP_SMTP_PASS,
+        },
+      });
+
+    const info = await transporter.sendMail({
+        from: 'Inngest TMS',  //TMS=Transport Mail Service
+        to,
+        subject,
+        text, // plain‑text body
+    });
+
+    console.log("Message sent:", info.messageId);
+    return info
+}
+catch(error){
+console.error(error,"Mail errrorr")
+throw error
+}
+}
