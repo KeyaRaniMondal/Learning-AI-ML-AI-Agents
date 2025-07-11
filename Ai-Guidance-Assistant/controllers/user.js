19b53c98269d97989f79324707f43b0cbc6b2003
Keya <keyamondal549@gmail.com>
2025-07-11 22:06:45 +0600

    created signup and login method

---
+++ Ai-Guidance-Assistant/controllers/user.js
@@ -0,0 +1,40 @@
+import { inngest } from "../Inngest/client"
+import User from "../models/user.model"
+//for signing up
+export const signup = async (req, res) => {
+    const { email, password, skills = [] } = req.body
+    try {
+        const hashed = becrypt.hash(password, 10)
+        const user = await User.create({ email, password: hashed, skills })
+        //trigger inngest event
+        await inngest.send({
+            name: "user/signup",
+            data: {
+                email
+            }
+        })
+        const token = Jwt.sign({ _id: user._id, role: user.role }.process.env.JWT_SECRET)
+        res.json({ user, token })
+    }
+    catch (error) {
+        res.status(500).json({ error: "Sign up failed", details: error.message })
+    }
+}
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = User.findOne({ email })
+        if (!user)
+            return res.status(401).json({ error: "User not found" })
+        const isMatched = await becrypt.compare(password, user.password)
+        if (!isMatched)
+            return res.status(401).json({ error: "Invalid credentials" })
+        const token = jwt.sign({ _id: user._id, role: user.role }.process.env.JWT_SECRET)
+        res.json({ user, token })
+    }
+    catch (error) {
+        res.status(500).json({ error: "Sign up failed", details: error.message })
+    }
+}
