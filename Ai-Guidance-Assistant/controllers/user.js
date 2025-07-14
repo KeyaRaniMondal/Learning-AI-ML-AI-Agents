9fb24b72360c82e065ed9dbc350ccaf462944740
Keya <keyamondal549@gmail.com>
2025-07-14 20:18:32 +0600

    completed get users method & all controllers

--- Ai-Guidance-Assistant/controllers/user.js
+++ Ai-Guidance-Assistant/controllers/user.js
@@ -79,3 +79,19 @@ export const updateUser = async (req, res) => {
     }
 
 }
+
+
+//for getting users
+
+export const getUsers = async (req, res) => {
+    try {
+        if (req.user.role != "admin") {
+            return res.status(403).json({ error: "Forbidden" })
+        }
+        const users = await User.find().select("-password")
+        return res.json(users)
+    }
+    catch (error) {
+        res.status(500).json({ error: "Logout failed", details: error.message })
+    }
+}
